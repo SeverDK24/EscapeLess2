687c726734be2f204f50dc057dd3343e37f2aa46
SeverDK24 <severyn.chugai@gmail.com>
2025-11-04 20:58:32 +0100

    GhostPatrol

--- Assets/scripts2lvl/enemy.cs
+++ Assets/scripts2lvl/enemy.cs
@@ -6,8 +6,12 @@ public class enemy : MonoBehaviour
 {
 
     [SerializeField] Transform target;
-    public int rad = 2;
+    public float rad = 1.4f;
     public bool isenter = false;
+    [SerializeField] Transform point;
+    [SerializeField] Transform point1;
+    public Transform point2;
+    public Transform point3;
     NavMeshAgent agent; 
     void Start()
     {
@@ -15,17 +19,23 @@ public class enemy : MonoBehaviour
         agent.updateRotation = false;
         agent.updateUpAxis = false; 
     }
-   //agent.SetDestination(target.position);
+   
    
     void Update()
     {
+        agent.SetDestination(point.position);
         Collider2D[] hits = Physics2D.OverlapCircleAll(transform.position,rad);
         foreach (Collider2D hit in hits)
         {
-            if (hit.gameObject.GetComponent<Reycast2LVL>()&& isenter)
+            if (hit.gameObject.GetComponent<Reycast2LVL>() && isenter)
             {
                 agent.SetDestination(target.position);
             }
+            //if ((!hit.gameObject.GetComponent<Reycast2LVL>()) && isenter)
+            //{
+            //    agent.SetDestination(point.position);
+            //}
+
         }
          
     }
@@ -36,6 +46,22 @@ public class enemy : MonoBehaviour
         Gizmos.color = Color.yellow;
         Gizmos.DrawWireSphere(transform.position,rad);
     }
-   
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "first")
+        {
+            agent.SetDestination(point1.position);
+        }
+
+        if (collision.gameObject.tag == "sec")
+        {
+            agent.SetDestination(point2.position);
+        }
+        if (collision.gameObject.tag == "thrd")
+        {
+            agent.SetDestination(point3.position);
+        }
+
+    }
 }
